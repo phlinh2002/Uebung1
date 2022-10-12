96921a8ed8ac29b5a025bc9a894b2ce2d2688bce
s0584555 <phlinhmcer2002@gmail.com>
2022-10-12 13:49:05 +0200

    Test

--- src/Uebung1.java
+++ src/Uebung1.java
@@ -1,8 +1,31 @@
-import java.awt.Graphics;
+import java.util.Scanner;
 public class Uebung1 {
-    public static void main(String[] args) {
-    System.out.print("hh");
+    public static  void main(String[] args) {
+        int prufen = 0;
+        String  [] name = new String[4];
+        String wort = "buch";
+        int a = wort.length();
+        for(int v = 0; v< a; v++){
+            name[v] = String.valueOf(wort.charAt(v));
+        }
+        System.out.print("Bitte einen Buchstabe geben: ");
+        Scanner add = new Scanner(System.in);
+        String buchstabe;
+        buchstabe = add.next();
+        System.out.println("Der eingelesene Buchstabe laut: " + buchstabe);
+        add.close();
+        for(int i = 0; i < 4; i++){
+            System.out.println(i);
+            if(buchstabe == name[i]){
+                System.out.println("Richtig:" + buchstabe);
+            }
+            else{
+                prufen++;
+            }
+        }
+        if (prufen+1 == wort.length()){
+            System.out.println("Probieren Sie nochmal");
+        }
+
     }
-
-
 }
